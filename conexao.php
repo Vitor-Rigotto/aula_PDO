3f3c2c8d10bbf42d9e12325d6a3fba2c4783533a
Vitor Rigotto <vitor.gino@gmail.com>
2019-11-01 19:26:50 -0300

    adicao de arquivos de conexao e criacao de tabela

---
+++ conexao.php
@@ -0,0 +1,26 @@
+<?php 
+
+$server 	= '127.0.0.1';
+$usuario 	= 'root';
+$senha 		= ' ';
+$database	= 'aula';
+$con = mysqli_connect($server,$usuario,$senha,$database);
+if (mysqli_connect_errno()){
+	// erro na conexão
+		echo "Erro de conexão =>".mysqli_connect_error();
+		echo "Criando database....<br>";
+		$con = mysqli_connect($server,$usuario,$senha);
+		if (!mysqli_connect_errno()){
+			$consultaSQL = 'CREATE database aula';
+			mysqli_query($con,$consultaSQL);
+			mysqli_close($con);
+			echo "Banco de Dados Criado com Sucesso!!<br>";
+		}
+
+}else{
+	// conexão com sucesso
+	echo "Conectado com Sucesso!!";
+} 
+
+
+ ?>
---
+++ tabela.php
@@ -0,0 +1,27 @@
+<?php 
+
+$server 	= '127.0.0.1';
+$usuario 	= 'root';
+$senha 		= ' ';
+$database	= 'aula';
+$con = mysqli_connect($server,$usuario,$senha,$database);
+if (mysqli_connect_errno()) {
+	echo "Falha ao conectar com o Banco de Dados!!";
+}else{
+	$comandoSQL = '
+					CREATE TABLE usuarios (
+					codigo INT(3) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+					nome VARCHAR(30) NOT NULL,
+					sobrenome VARCHAR(30) NOT NULL,
+					email VARCHAR(50),
+					senha VARCHAR(32),
+					horario TIMESTAMP
+					)
+				';
+	mysqli_query($con,$comandoSQL);
+	mysqli_close($con);
+
+}
+
+
+ ?>
